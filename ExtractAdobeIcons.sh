ea087298eb7a730aafe696b1ec5c2539379813fe
A.Y. Siu <aysiu@users.noreply.github.com>
2016-11-04 10:07:11 -0700

    Add check for missing file extension

--- ExtractAdobeIcons.sh
+++ ExtractAdobeIcons.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 # Extracts .png icons from installed Adobe Creative Cloud .app bundles
-# Has to be run on the Mac that actually has the relevant .app bundles already installed
 
 # Find the Adobe .app bundles
 # Exclude the general Adobe directory as well as the one in Utilities
 find /Applications -name "Adobe *.app" -maxdepth 2 -not -path "/Applications/Adobe/*" -not -path "/Applications/Utilities*" | while read line; do
    # Get the icon name
    icnsName=$(/usr/libexec/PlistBuddy -c "print :CFBundleIconFile" "$line"/Contents/Info.plist)
+   # Sometimes Adobe leaves the file extension off the icon name, so check the extension is there
+   fileExtension="${icnsName##*.}"
+   if [ "${icnsName##*.}" != "icns" ]; then
+      # If it's not there, append the extension
+      icnsName+='.icns'
+   fi
    # Get the app name
    appName=$(/usr/libexec/PlistBuddy -c "print :CFBundleName" "$line"/Contents/Info.plist)
    # Create a .png of the .icns on the desktop

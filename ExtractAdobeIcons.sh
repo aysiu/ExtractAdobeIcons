0c9c20490573daffd21b0b67be0c2b96749a2eb8
A.Y. Siu <aysiu@users.noreply.github.com>
2016-11-04 10:11:41 -0700

    Added full paths to binaries

--- ExtractAdobeIcons.sh
+++ ExtractAdobeIcons.sh
@@ -4,7 +4,7 @@
 
 # Find the Adobe .app bundles
 # Exclude the general Adobe directory as well as the one in Utilities
-find /Applications -name "Adobe *.app" -maxdepth 2 -not -path "/Applications/Adobe/*" -not -path "/Applications/Utilities*" | while read line; do
+/usr/bin/find /Applications -name "Adobe *.app" -maxdepth 2 -not -path "/Applications/Adobe/*" -not -path "/Applications/Utilities*" | while read line; do
    # Get the icon name
    icnsName=$(/usr/libexec/PlistBuddy -c "print :CFBundleIconFile" "$line"/Contents/Info.plist)
    # Sometimes Adobe leaves the file extension off the icon name, so check the extension is there
@@ -16,5 +16,5 @@ find /Applications -name "Adobe *.app" -maxdepth 2 -not -path "/Applications/Ado
    # Get the app name
    appName=$(/usr/libexec/PlistBuddy -c "print :CFBundleName" "$line"/Contents/Info.plist)
    # Create a .png of the .icns on the desktop
-   sips -s format png "$line"/Contents/Resources/"$icnsName" --out ~/Desktop/"$appName".png
+   /usr/bin/sips -s format png "$line"/Contents/Resources/"$icnsName" --out ~/Desktop/"$appName".png
 done

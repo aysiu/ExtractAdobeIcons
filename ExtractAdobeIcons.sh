632f70d652d2f8a21f580c72f8bdb1f836f6078a
A.Y. Siu <aysiu@users.noreply.github.com>
2016-11-04 10:25:50 -0700

    Fixed missing CFBundleName
    Gets app name from the bundle name if the CFBundleName does not exist

--- ExtractAdobeIcons.sh
+++ ExtractAdobeIcons.sh
@@ -4,7 +4,7 @@
 
 # Find the Adobe .app bundles
 # Exclude the general Adobe directory as well as the one in Utilities
-/usr/bin/find /Applications -name "Adobe *.app" -maxdepth 2 -not -path "/Applications/Adobe/*" -not -path "/Applications/Utilities*" | while read line; do
+find /Applications -name "Adobe *.app" -maxdepth 2 -not -path "/Applications/Adobe/*" -not -path "/Applications/Utilities*" | while read line; do
    # Get the icon name
    icnsName=$(/usr/libexec/PlistBuddy -c "print :CFBundleIconFile" "$line"/Contents/Info.plist)
    # Sometimes Adobe leaves the file extension off the icon name, so check the extension is there
@@ -15,6 +15,10 @@
    fi
    # Get the app name
    appName=$(/usr/libexec/PlistBuddy -c "print :CFBundleName" "$line"/Contents/Info.plist)
+   # Check that the app name isn't empty. For some reason, sometimes Adobe doesn't include the CFBundleName key
+   if [ -z "$appName" ]; then
+      appName=$(/usr/bin/basename "$line" .app)
+   fi
    # Create a .png of the .icns on the desktop
-   /usr/bin/sips -s format png "$line"/Contents/Resources/"$icnsName" --out ~/Desktop/"$appName".png
+   sips -s format png "$line"/Contents/Resources/"$icnsName" --out ~/Desktop/"$appName".png
 done
